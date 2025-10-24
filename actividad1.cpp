67b0cde03fc7eb8e5ab2a19df69e06c46cdd352f
Juan David Troncoso <juandavid.troncoso@gmail.com>
2025-10-24 09:00:54 -0500

    Remove commented instructions from actividad1.cpp for cleaner code presentation

--- actividad1.cpp
+++ actividad1.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 int main() {
-    // 1. Crear un programa en C++, declarar y asignar una variable entera (int)
     int variable = 42;
     
-    // 2. Presentar en pantalla la dirección de memoria de esa variable
     cout << "=== ACTIVIDAD No. 1 ===" << endl;
     cout << "\n1. Variable inicial:" << endl;
     cout << "   Valor: " << variable << endl;
     cout << "   Direccion de memoria: " << &variable << endl;
     
-    // 3. Modificar el valor de la variable indirectamente utilizando punteros
     int* puntero = &variable;
     *puntero = 100;
     
-    // 4. Presentar nuevamente el valor de la variable y la dirección de memoria
     cout << "\n2. Despues de modificar con puntero:" << endl;
     cout << "   Nuevo valor: " << variable << endl;
     cout << "   Valor a traves del puntero: " << *puntero << endl;
